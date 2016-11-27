662a3fd1d1f9438ee7fd325da1773841f3dc25cb
prakashseela <prakashseela@192.168.1.7>
2016-11-27 14:37:25 +0530

    Initial commit

---
+++ src/main/java/reactor/boot/start/ReactorBootApplication.java
@@ -0,0 +1,12 @@
+package reactor.boot.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorBootApplication.class, args);
+	}
+}
